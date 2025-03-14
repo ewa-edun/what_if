e954f0e00043ea5b8edb278ff80124aee30f5ad1
Ewa <ewa.edun@gmail.com>
2025-03-14 12:47:53 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <p>&copy; {new Date().getFullYear()} What If? AI - The Alternate Reality Generator</p>
+        <p>&copy; {new Date().getFullYear()} What If? - The AI Alternate Reality Generator</p>
       </div>
     </footer>
   )
